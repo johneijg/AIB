0101e16ed3a4e11bab609a6191feb5be0bfb888b
John Eijgensteijn <50658225+johneijg@users.noreply.github.com>
2022-09-20 23:27:12 +0200

    remove update mdule

--- AMV-INSTALL.ps1
+++ AMV-INSTALL.ps1
@@ -1,13 +1,6 @@
 Write-Host  -ForegroundColor Cyan "Gemeente Amstelveen Werkplek 365 - Stap 1: Reset van Windows"
 Start-Sleep -Seconds 5
 
-#Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
-Install-Module OSD -Force -SkipPublisherCheck
-
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with Amstelveen Parameters"
 Start-OSDCloud -OSName 'Windows 10 21H2 x64'-OSLanguage nl-nl -OSEdition Pro -ZTI -Restart
